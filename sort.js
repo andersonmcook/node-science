0056c18f2089adaa62180ea8e2af7b48bacab55e
andersonmcook <andersonmcook@gmail.com>
2016-03-09 11:12:31 -0600

    insertion sort v1

--- sort.js
+++ sort.js
@@ -1,10 +1,12 @@
 'use strict'
 
 const chalk = require('chalk')
+const util = require('util') // built into node
 
-// const arr = [9,6,5,3,1,8,7,2,4]
-const arr = [8,7,6,5,4,3,2,1]
-console.log(chalk.yellow(arr))
+// const arr = [6,5,3,1,8,7,2,4]
+const arr = [7,0,4,1,8,6,9,3,2,5]
+
+console.log(arr)
 
 function swap (array, index1, index2) {
   const temp = array[index1]
@@ -15,18 +17,35 @@ function swap (array, index1, index2) {
 
 function bubbleSort (array) {
   for (let j = 1; j < array.length; j++) {
-    console.log(chalk.red(j))
     for (let i = 0; i < array.length - j; i++) {
       if (array[i] > array[i + 1]) {
         swap(array, i, i + 1)
-        // console.log(chalk.red(array))
+        console.log(chalk.red(util.inspect(array)))
       }
-    console.log(chalk.cyan(array))
+      console.log(chalk.grey(util.inspect(array)))
     }
   }
-
 
   return array
 }
 
-console.log(chalk.green(bubbleSort(arr)))
+function insertionSort (array) {
+  for (let j = 1; j < array.length; j++) {
+    for(let i = 0; i < array.length - j; i++) {
+      if (array[i] > array[i + 1]) {
+        swap(array, i, i + 1)
+        console.log(chalk.yellow(util.inspect(array)))
+        while (array[i] < array[i - 1]) {
+          swap(array, i, i - 1)
+          i--
+          console.log(chalk.red(util.inspect(array)))
+        }
+      }
+    }
+  }
+  return array
+}
+
+// console.log('bubbled', bubbleSort(arr))
+console.log(insertionSort(arr))
+
